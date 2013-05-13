bc4f6e659a2b22458feec163f48387e41282bd82
Eric Normandeau <eric.normandeau.qc@gmail.com>
2013-05-13 13:21:47 -0400

    Updated scripts documentation

--- 00-scripts/stacks_1b_pstacks.sh
+++ 00-scripts/stacks_1b_pstacks.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-
-# Works with version 0.99995
+# Launch pstacks to treat all the samples individually
+# Tested with version 0.99999
 
 # Options
 # Comment out options that you do not wish to use
 
-t="-t sam"            #t: input file Type. Supported types: bowtie, sam, or bam.
-#f="-f ./03-all_samples"   #f: input file path.
-o="-o 05-stacks-c"        #o: output path to write results.
-#i="-i 1"                #i: SQL ID to insert into the output to identify this sample.
-m="-m 2"                #m: minimum depth of coverage to report a stack (default 1).
-p="-p 3"               #p: enable parallel execution with num_threads threads.
+t="-t sam"            # t: input file Type. Supported types: bowtie, sam, or bam
+o="-o 05-stacks-c"    # o: output path to write results.
+#i="-i 1"             # i: SQL ID to insert into the output to identify this
+                      #   sample
+m="-m 2"              # m: minimum depth of coverage to report a stack
+                      #   (default 1)
+p="-p 3"              # p: enable parallel execution with num_threads threads
 
 #max_locus_stacks="--max_locus_stacks 3"
 model_type="--model_type snp"
@@ -19,8 +20,6 @@ alpha="--alpha 0.05"
 #bound_high="--bound_high 1"
 #bc_err_freq="--bc_err_freq 1"
 
-
-#pstacks $t $o $i $m $p $max_locus_stacks $model_type $alpha $bound_low $bound_high $bc_err_freq $f
-
+# Launch pstacks for all the individuals
 id=1
 for file in 04c-all_samples/*.sam; do echo -e "\n\n##### Treating individual $id: $file\n\n"; pstacks $t $o $i $m $p $max_locus_stacks $model_type $alpha $bound_low $bound_high $bc_err_freq -f $file -i $id; id=$(echo $id + 1 | bc); done
