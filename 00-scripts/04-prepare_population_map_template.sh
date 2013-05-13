04c389832e6511993caf19a0ebe1d483cef2fa87
Eric Normandeau <eric.normandeau.qc@gmail.com>
2013-05-13 16:00:24 -0400

    Updated scripts after complete test

--- 00-scripts/04-prepare_population_map_template.sh
+++ 00-scripts/04-prepare_population_map_template.sh
@@ -5,5 +5,6 @@
 ls -1 04-all_samples/ |
     while read i
     do
-        echo -e "$i\t1"
+        echo -e "$i\t1" |
+        perl -pe 's/\.fq//'
     done > 01-info_files/population_map_template.txt
