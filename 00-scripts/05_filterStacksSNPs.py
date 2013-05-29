f48b03e0df6ceda3bb75f52b82ead3c3df661c11
Eric Normandeau <eric.normandeau.qc@gmail.com>
2013-05-29 15:12:12 -0400

    Fixed conflict between master and v0.1.1

--- 00-scripts/05_filterStacksSNPs.py
+++ 00-scripts/05_filterStacksSNPs.py
@@ -2,12 +2,7 @@
 """Filter sumstats.tsv files from STACKS to remove :
 
 Usage:
-<<<<<<< HEAD
-  ./filterStacksSNPs.py inputFile maxSnpNumber maxAlleleNumber minPresence \
-       maxHetero minAlleleFreq minFis maxFis
-=======
   ./filterStacksSNPs.py inFile maxSnpNumber maxAlleleNumber minPresence maxHetero minAlleleFreq minFis maxFis
->>>>>>> v0.1.1
 
 inFile = batch_1.sumstat.tsv or similarly names output of STACKS (v0.99995+)
 maxSnpNumber = maximum number of SNPs in a single locus (int, 1 or more)
