730ac70c5d4457eb0dd6c666af650b974bf459a8
Eric Normandeau <eric.normandeau.qc@gmail.com>
2013-07-30 14:07:03 -0400

    Renaming sample files using population and individual names

--- 00-scripts/03_rename_samples.sh
+++ 00-scripts/03_rename_samples.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 # Renaming the extracted sample files
 
+# Global variables
+INFO_FILES="01-info_files"
+SAMPLES_FOLDER="03-samples"
+ALL_SAMPLES_FOLDER="04-all_samples"
+
 # Renaming files
-cat 01-info_files/lane_info.txt |
+cat $INFO_FILES/lane_info.txt |
     while read file
     do
-        for s in $(ls -1 03-samples/$file/sample*)
+        for s in $(ls -1 $SAMPLES_FOLDER/$file/sample* 2> /dev/null)
         do
             sample=$(basename $s)
-            mv 03-samples/$file/$sample 03-samples/$file/$file"_"$sample
+            mv $SAMPLES_FOLDER/$file/$sample $SAMPLES_FOLDER/$file/$file"_"$sample
         done
     done
 
 # Linking files
-cat 01-info_files/lane_info.txt |
+cat $INFO_FILES/lane_info.txt |
     while read file
     do
-        for s in $(ls -1 03-samples/$file/*.fq)
+        for sample_file in $(ls -1 $SAMPLES_FOLDER/$file/*.fq)
         do
-            sample=$(basename $s)
-            cp -l 03-samples/$file/$sample 04-all_samples
+            lane=$file
+            barcode=$(echo $sample_file | perl -pe 's/^.*_sample_//; s/\.fq//')
+            sample_info=$(grep $lane $INFO_FILES/sample_information.txt | grep $barcode)
+            population=$(echo $sample_info | cut -d " " -f 3)
+            sample_name=$(echo $sample_info | cut -d " " -f 4)
+            new_name=$(echo "$population"_"$sample_name".fq)
+            cp -l $sample_file $ALL_SAMPLES_FOLDER/$new_name
         done
     done
 
