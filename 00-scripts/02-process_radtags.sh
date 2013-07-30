0d6003163e86f76eda5ca6cf531e2b561f15ab99
Eric Normandeau <eric.normandeau.qc@gmail.com>
2013-07-30 11:45:17 -0400

    Extract read based on lane barcodes only

--- 00-scripts/02-process_radtags.sh
+++ 00-scripts/02-process_radtags.sh
@@ -7,12 +7,18 @@ TRIM_LENGTH=$1
 # Name of the enzyme (run 'process_radtags', without options, for list)
 ENZYME=$2
 
-# Prepare bacode_lengths.txt from barcodes.txt, 
-perl -ne 'chomp; print length."\n"' 01-info_files/barcodes.txt |
-    sort -un > 01-info_files/barcode_lengths.txt
+cat 01-info_files/lane_info.txt |
+while read f
+do
+    grep -E "$f" 01-info_files/sample_information.txt | \
+        cut -f 2 > 01-info_files/barcodes.txt
 
-# Create barcode files, eg: barcodes_4.txt, barcodes_5.txt ...
-cat 01-info_files/barcode_lengths.txt |
+    # Prepare bacode_lengths.txt from barcodes.txt 
+    perl -ne 'chomp; print length."\n"' 01-info_files/barcodes.txt |
+        sort -un > 01-info_files/barcode_lengths.txt
+
+    # Create barcode files, eg: barcodes_4.txt, barcodes_5.txt ...
+    cat 01-info_files/barcode_lengths.txt |
     while read b
     do
         perl -sane 'chomp; if (length eq $b) {print $_."\n"}'\
@@ -20,22 +26,15 @@ cat 01-info_files/barcode_lengths.txt |
         sort -u > 01-info_files/barcodes_$b".txt"
     done
 
-# Comment 
-cat 01-info_files/lane_info.txt |
-    while read f
+    # Extract reads for each individuals using barcodes
+    cat 01-info_files/barcode_lengths.txt |
+    while read b
     do
-        # Get barcodes for that line, put in barcodes_current_lane.txt
-        # Prepare barcode_lengths.txt from barcodes_current_lane.txt
-        # Create barcode files
-        # Use the -D option to filter only on discarted reads
-        cat 01-info_files/barcode_lengths.txt |
-        while read b
-        do
-            mkdir 03-samples/$f
-            process_radtags -i gzfastq -f 02-raw/$f".fastq.gz" -o \
-                03-samples/$f -b 01-info_files/barcodes_"$b".txt \
-                -c -q -r -t $TRIM_LENGTH --barcode_dist\
-                --filter_illumina 3 -E phred33 -e $ENZYME
-        done
+        mkdir 03-samples/$f
+        process_radtags -i gzfastq -f 02-raw/$f".fastq.gz" -o \
+            03-samples/$f -b 01-info_files/barcodes_"$b".txt \
+            -c -q -r -t $TRIM_LENGTH --barcode_dist \
+            --filter_illumina 3 -E phred33 -e $ENZYME
     done
+done
 
