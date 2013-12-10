191076185307d74d47e2678340e20ced99aa5100
Eric Normandeau <eric.normandeau.qc@gmail.com>
2013-12-10 13:19:58 -0500

    Remove old script

--- 00-scripts/utility_scripts/align.sh
+++
@@ -1,1 +0,0 @@
-for i in $(ls -1 04-all_samples/*.fq); do name=$(basename $i);bwa aln -n 3 -k 1 -t 6 ./01-info_files/Gasterosteus_aculeatus.fa $i | bwa samse -r "@RG\tID:'$name'\tSM:'$name'\tPL:Illumina" ./01-info_files/Gasterosteus_aculeatus.fa - $i > ./04-all_samples/$name.sam;done
