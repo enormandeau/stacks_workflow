72a1fc967e71af166f895c4ca71bc490ca84bf94
Eric Normandeau <eric.normandeau.qc@gmail.com>
2013-12-17 12:00:26 -0500

    Fixed bug in 03_rename_samples.sh

--- 00-scripts/03_rename_samples.sh
+++ 00-scripts/03_rename_samples.sh
@@ -23,23 +23,13 @@ cat $INFO_FILES/lane_info.txt |
     do
         for sample_file in $(ls -1 $SAMPLES_FOLDER/$lane/*.fq)
         do
-            echo "--new sample--"
-            echo "Sample file: $sample_file"
-            echo "lane: $lane"
             barcode=$(echo $sample_file | perl -pe 's/^.*_sample_//; s/\.fq//')
-            echo "barcode: $barcode"
-            #grep $lane $INFO_FILES/sample_information.csv | grep -E "[[:space:]]$barcode[[:space:]]"
             sample_info=$(grep $lane $INFO_FILES/sample_information.csv | grep -E "[[:space:]]$barcode[[:space:]]")
-            echo "sample_info: $sample_info"
             population=$(echo $sample_info | cut -d " " -f 3)
-            echo "population: $population"
             sample_name=$(echo $sample_info | cut -d " " -f 4)
-            echo "sample_name: $sample_name"
             new_name=$(echo "$population"_"$sample_name".fq)
-            echo "new_name: $new_name"
 
             cp -l $sample_file $ALL_SAMPLES_FOLDER/$new_name
-            echo
         done
     done
 
