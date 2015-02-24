001bc7bd1c4a22dcc18e0316a1095ce255963722
Eric Normandeau <eric.normandeau.qc@gmail.com>
2015-02-24 15:48:06 -0500

    Merged Thierry's patches for version 1.25

--- 00-scripts/stacks_8_populations_rx.sh
+++ 00-scripts/stacks_8_populations_rx.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # Launch populations
 
-# make sure your read available options for populations in the papers and
-# comment in/out options required to run your analysis
+# IMPORTANT: make sure your read about the available options for 'populations'
+# in the STACKS papers
+
+# Comment out the options required to run your analysis
 
 b="-b 1"               # Batch ID to examine when exporting from the catalog
 P="-P 05-stacks_rx"    # Path to the Stacks output files.
-M="-M 01-info_files/population_map.txt"  # Path to the population map, a 
-                                         # tab separated file describing 
+M="-M 01-info_files/population_map.txt"  # Path to the population map, a
+                                         # tab separated file describing
                                          # which individuals belong in
                                          # which population
 #s="-s file_for_sql"  # Output a file to import results into an SQL database
@@ -16,33 +18,33 @@ M="-M 01-info_files/population_map.txt"  # Path to the population map, a
 #W="-W whitelist_file.txt"   # Specify a file containing Whitelisted markers
                              # to include in the export
 #e="-e ENZYME"   # Restriction enzyme, required if generating 'genomic' output
-t="-t 8"       # Number of threads to run in parallel sections of code
+t="-t 8"         # Number of threads to run in parallel sections of code
 #v="-v"          # Print program version.
 #h="-h"          # Display this help message.
 
 # Merging and Phasing:
-#merge_sites="--merge_sites"    # merge loci that were produced from the same 
+#merge_sites="--merge_sites"   # merge loci that were produced from the same
                                # restriction enzyme cutsite
                                # (requires reference-aligned data).
-#merge_prune_lim="--merge_prune_lim"   # when merging adjacent loci, 
-                                      # if at least X% samples posses both loci 
+#merge_prune_lim="--merge_prune_lim"   # when merging adjacent loci,
+                                      # if at least X% samples posses both loci
                                       # prune the remaining samples out of the analysis.
 # Data filtering
 r="-r 0.8"          # Minimum percentage of individuals in a population
                     # required to process a locus for that population
-p="-p 1"           # Minimum number of populations a locus must be present
+p="-p 1"            # Minimum number of populations a locus must be present
                     # in order to process a locus
-m="-m 4"           # Specify a minimum stack depth required for individuals
+m="-m 4"            # Specify a minimum stack depth required for individuals
                     # at a locus
 a="-a 0.05"         # Specify a minimum minor allele frequency required
                     # before calculating Fst at a locus (0 < a < 0.5)
 f="-f p_value"      # Specify a correction to be applied to Fst values:
                     # 'p_value', 'bonferroni_win', or 'bonferroni_gen'
-p_value_cutoff="--p_value_cutoff 0.05"  # Required p-value to keep an Fst 
-                                        # measurement (0.05 by default). Also 
-                                        # used as base for Bonferroni 
+p_value_cutoff="--p_value_cutoff 0.05"  # Required p-value to keep an Fst
+                                        # measurement (0.05 by default). Also
+                                        # used as base for Bonferroni
                                         # correction
-lnl_lim="--lnl_lim -5"   # Filter loci with log likelihood values below this 
+lnl_lim="--lnl_lim -5"   # Filter loci with log likelihood values below this
                          # threshold.
 #write_single_snp="--write_single_snp"  # write only the first SNP per locus in
                                         # Genepop and Structure outputs
@@ -54,24 +56,24 @@ lnl_lim="--lnl_lim -5"   # Filter loci with log likelihood values below this
 
 # Kernel-smoothing algorithm
 #k="-k"                # enable kernel-smoothed Pi, Fis, Fst, Fst', and Phi_st calculations
-#window_size="--window_size 150Kb"       # distance over which to average values  
+#window_size="--window_size 150Kb"       # distance over which to average values
                                          #(sigma, default 150Kb)
 # Bootstrap resampling
 #bootstrap="--bootstrap"               # turn on broostrap resampling
                                        # for all smoothed statistics.
 #bootstrap_pifis="--bootstrap_pifis"   # turn on boostrap resampling
-                                       # for smoothed SNP-based Pi 
+                                       # for smoothed SNP-based Pi
                                        # and Fis calculations.
-#bootstrap_fst="--bootstrap_fst"       # turn on boostrap resampling 
-                                       # for smoothed Fst calculations 
-                                       # based on pairwise population 
+#bootstrap_fst="--bootstrap_fst"       # turn on boostrap resampling
+                                       # for smoothed Fst calculations
+                                       # based on pairwise population
                                        # comparison of SNPs.
-#bootstrap_div="--bootstrap_div"       # turn on boostrap resampling 
+#bootstrap_div="--bootstrap_div"       # turn on boostrap resampling
                                        # for smoothed haplotype diveristy
                                        # and gene diversity calculations
                                        # based on haplotypes.
 #bootstrap_phist="--bootstrap_phist"   # turn on boostrap resampling
-                                       # for smoothed Phi_st calculations 
+                                       # for smoothed Phi_st calculations
                                        # based on haplotypes.
 #bootstrap_reps="--bootstrap_reps 100" # number of bootstrap resamplings
                                        # to calculate (default 100).
@@ -81,10 +83,10 @@ lnl_lim="--lnl_lim -5"   # Filter loci with log likelihood values below this
 # file output options
 #genomic="--genomic"               # Output each nucleotide position
                                    # (fixed or polymorphic) in all population
-                                   # members to a file
+                                   # members to a file.
 #fasta="--fasta"                   # Output full sequence for each allele,
                                    # from each sample locus in FASTA format.
-#vcf="--vcf"                       # Output results in Variant Call Format (VCF)
+#vcf="--vcf"                       # Output results in Variant Call Format (VCF).
 #vcf_haplotypes="--vcf_haplotypes" # Output haplotypes in Variant Call Format (VCF).
 #genepop="--genepop"               # Output results in GenePop format
 #structure="--structure"           # Output results in Structure format
@@ -94,10 +96,10 @@ lnl_lim="--lnl_lim -5"   # Filter loci with log likelihood values below this
 #beagle_phased="--beagle_phased"   # Output haplotypes in Beagle format.
 #plink="--plink"                   # Output genotypes in PLINK format.
 #phylip="--phylip"                 # Output nucleotides that are fixed-within
-                                   # and variant among populations in Phylip 
+                                   # and variant among populations in Phylip
                                    # format for phylogenetic tree construction
 #phylip_var="--phylip_var"         # Include variable sites in the phylip output
-#hzar="--hzar"                     # Output genotypes in Hybrid Zone Analysis 
+#hzar="--hzar"                     # Output genotypes in Hybrid Zone Analysis
                                    # using R (HZAR) format.
 
 # Debugging
