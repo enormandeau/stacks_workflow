f3bd530d86af47440006883649cc49b716a92b1a
Eric Normandeau <eric.normandeau.qc@gmail.com>
2015-07-21 15:17:15 -0400

    Added katak job submission scripts

---
+++ 00-scripts/katak_jobs/stacks_4_populations_job.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#$ -N AX_4_population
+#$ -M your.addresse@service.com
+#$ -m beas
+#$ -pe smp 4
+#$ -l h_vmem=100G
+#$ -l h_rt=40:00:00
+#$ -cwd
+#$ -S /bin/bash
+
+#./00-scripts/00_prepare_lane_info.sh
+#./00-scripts/01_cutadapt.sh
+#./00-scripts/02_process_radtags_2_enzymes.sh 80 pstI mspI
+#./00-scripts/02_process_radtags.sh
+#./00-scripts/03_rename_samples_complex.sh
+#./00-scripts/03_rename_samples_simple.sh
+#./00-scripts/04_prepare_population_map.sh
+#./00-scripts/05_filter_vcf.py
+#./00-scripts/stacks_1a_ustacks.sh
+#./00-scripts/stacks_1b_pstacks.sh
+#./00-scripts/stacks_2_cstacks.sh
+#./00-scripts/stacks_3_sstacks.sh
+./00-scripts/stacks_4_populations.sh
+#./00-scripts/stacks_5a_rxstacks_likelihoods.sh
+#./00-scripts/stacks_5b_rxstacks.sh
+#./00-scripts/stacks_6_cstacks_rx.sh
+#./00-scripts/stacks_7_sstacks_rx.sh
+#./00-scripts/stacks_8_populations_rx.sh
+
