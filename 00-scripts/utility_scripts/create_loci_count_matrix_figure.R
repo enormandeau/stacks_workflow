f4316ed11d708d5db2994193b6b0e58ac3b0bcc5
Eric Normandeau <eric.normandeau.qc@gmail.com>
2015-10-15 13:47:32 -0400

    Improve diagnostic of bad individuals

--- 00-scripts/utility_scripts/create_loci_count_matrix_figure.R
+++ 00-scripts/utility_scripts/create_loci_count_matrix_figure.R
@@ -2,7 +2,11 @@
 rm(list=ls())
 
 # Global variables
-data.file = "count_matrix.tsv"
+proportion.used = 0.2
+data.file = paste("count_matrix", proportion.used, "present.tsv", sep="_")
+heatmap.file = paste("count_figure", proportion.used, "present.jpg", sep="_")
+barplot.file = paste("barplot", proportion.used, "missing.jpg", sep="_")
+missing.file = paste("proportion", proportion.used, "missing.tsv", sep="_")
 
 # Trick to speedup loading the data
 temp = read.table(data.file, header=T, nrows=5)
@@ -14,9 +18,23 @@ rm(temp)
 data = read.table(data.file, header=T, sep="\t", colClasses=classes)
 
 # Use subset to create heatmap
-d = data[1:400, ]
+d = log(data[1:400,] + 1)
 
-jpeg("count_figure_all_present.jpg")
-    heatmap(as.matrix(d))
+jpeg(heatmap.file, width=2000, height=2000)
+    heatmap(as.matrix(d), scale="none", Rowv=NA, Colv=NA)
 dev.off()
+
+# Barplot of missing data per individual
+res = c()
+for (i in 1:ncol(data)) {
+    res = c(res, length(data[data[,i] == 0.0,i]) / nrow(data))
+}
+
+jpeg(barplot.file, width=800, height=800)
+    barplot(res, ylim=c(0, 1))
+dev.off()
+
+# Write file with individual proportion of missing data
+missing = data.frame(indName=names(data), propMissing=res)
+write.table(missing, missing.file, sep="\t")
 
