4bb53e81500f08e224be0c4acd561e1b6f53303b
Eric Normandeau <eric.normandeau.qc@gmail.com>
2015-11-30 09:50:25 -0500

    Parallelize sstacks

--- 00-scripts/stacks_7_sstacks_rx.sh
+++ 00-scripts/stacks_7_sstacks_rx.sh
@@ -10,8 +10,7 @@ LOG_FOLDER="98-log_files"
 cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"
 
 # OPTIONS: Comment out options that you do not wish to use
-p="-p 16"                  # p: enable parallel execution with num_threads
-                           #   threads
+p="-p 16"                  # p: enable parallel execution with num_threads threads
 b="-b 1"                   # b: MySQL ID of this batch
 c="-c 05-stacks_rx/batch_1"   # c: TSV file from which to load the catalog loci
 o="-o 05-stacks_rx"           # o: output path to write results
@@ -19,9 +18,10 @@ o="-o 05-stacks_rx"           # o: output path to write results
                            #   identity
 #x="-x"                    # x: don't verify haplotype of matching locus
 
+# Prepare list of samples to treat
+s="$(for file in $(ls -1 05-stacks_rx/*.tags.tsv.gz | grep -v catalog | \
+    perl -pe 's/\.tags\.tsv\.gz//'); do echo -s $file; done)"
+
 # Launch sstacks on all samples
-for file in $(ls -1 05-stacks_rx/*.tags.tsv.gz | grep -v catalog | perl -pe 's/\.tags\.tsv\.gz//')
-do
-    sstacks $p $b $c $r $o $g $x $h -s $file
-done 2>&1 | tee 98-log_files/"$TIMESTAMP"_stacks_7_sstacks_rx.log
+sstacks $p $b $c $s $o $g $x $v $h 2>&1 | tee 98-log_files/"$TIMESTAMP"_stacks_7_sstacks_rx.log
 
