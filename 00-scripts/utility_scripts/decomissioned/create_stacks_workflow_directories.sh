451122c5379351ef2843e03f9159e2ba5682e15e
Eric Normandeau <eric.normandeau.qc@gmail.com>
2016-01-28 14:36:17 -0500

    Clean up old scripts and unneeded documentation files

--- 00-scripts/utility_scripts/decomissioned/create_stacks_workflow_directories.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Create directories needed for the gbs_workflow
-
-mkdir 01-info_files 00-scripts 02-raw 03-samples 04-all_samples 04aln-all_samples 05-stacks
-
--- 00-scripts/utility_scripts/decomissioned/remove_all_mids.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for i in `ls -1 *.fa`
-do
-    echo "Treating file $i"
-    ../remove_mid.py $i ../barcodes.txt $i.no_barcode
-done
-
