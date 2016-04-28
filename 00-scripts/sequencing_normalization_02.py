81c00c4ef5a335f2b665a4f6c2ecc531859c9328
Eric Normandeau <eric.normandeau.qc@gmail.com>
2016-04-28 09:05:18 -0400

    Improve layout of normalization .xls file

--- 00-scripts/sequencing_normalization_02.py
+++ 00-scripts/sequencing_normalization_02.py
@@ -10,6 +10,11 @@ Where:
 """
 
 # Modules
+# Excel support
+from xlrd import open_workbook
+from xlutils.copy import copy
+
+# Other modules
 from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -21,6 +26,29 @@ import os
 # Classes
 
 # Functions
+def _getOutCell(outSheet, colIndex, rowIndex):
+    """ HACK: Extract the internal xlwt cell representation. """
+    row = outSheet._Worksheet__rows.get(rowIndex)
+    if not row: return None
+
+    cell = row._Row__cells.get(colIndex)
+    return cell
+
+def setOutCell(outSheet, col, row, value):
+    """ Change cell value without changing formatting. """
+    # HACK to retain cell style.
+    previousCell = _getOutCell(outSheet, col, row)
+    # END HACK, PART I
+
+    outSheet.write(row, col, value)
+
+    # HACK, PART II
+    if previousCell:
+        newCell = _getOutCell(outSheet, col, row)
+        if newCell:
+            newCell.xf_idx = previousCell.xf_idx
+    # END HACK
+
 
 # Parsing user input
 try:
@@ -57,6 +85,7 @@ for chip in chips:
     data = pd.merge(info_df, numseq_df, on="Barcode")
 
     # Computations
+    sum_reads = sum(data["NumReads"])
     data["Missing"] = targetNumReads - data["NumReads"]
     data.loc[data["NumReads"] > targetNumReads, "Missing"] = 0
     data.loc[data["NumReads"] < minimumReads, "Missing"] = 0
@@ -70,10 +99,27 @@ for chip in chips:
     rows = list("ABCDEFGH")
     columns = range(1, 13)
 
+    # Open Excel template, get link to first sheet and print name of chip
+    rb = open_workbook("01-info_files/normalization_template.xls", formatting_info=True)
+    wb = copy(rb)
+    s = wb.get_sheet(0)
+
+    # Print some useful informations (chip name, some stats...)
+    setOutCell(s, 6, 0, chip)
+    setOutCell(s, 2, 10, "{0:.2f} million usable reads produced".format(
+        sum_reads / 1000000.))
+    setOutCell(s, 2, 11, "{0:.1f} million reads still needed to reach {1:.1f} million reads per sample".format(
+        float(data.shape[0]) * (float(targetNumReads) - sum_reads / float(data.shape[0])) / 1000000.0,
+        targetNumReads / 1000000.0))
+    setOutCell(s, 2, 12, "{0:.2f} more chips needed".format(
+        (float(data.shape[0]) * targetNumReads - sum_reads) / float(sum_reads)))
+
+    # Create empty plate
     plate = pd.DataFrame(np.zeros([8, 12]))
     plate.columns = columns
     plate.index = rows
 
+    # Fill plate with data
     for well in data["Well"]:
         row = well[0]
         column = int(well[1:])
@@ -81,5 +127,14 @@ for chip in chips:
         volume = "{0:.1f}".format(round(volume, 1))
         plate.loc[row, column] = volume
 
-    print plate
+    # Fill Excel template
+    nrow, ncol = plate.shape
+    for row in range(nrow):
+        for col in range(ncol):
+            setOutCell(s, col+1, row+2, float(plate.iloc[row, col]))
+
+    # Write CSV file
     plate.to_csv(os.path.join(folder, chip + "_normalization.csv"), sep="\t")
+
+    # Write filled Excel template
+    wb.save(chip + "_normalization.xls")
