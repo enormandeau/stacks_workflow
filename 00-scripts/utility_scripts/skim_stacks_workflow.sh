03a4879c537c99694be392b43fbda5dc4fa133e0
Eric Normandeau <eric.normandeau.qc@gmail.com>
2016-05-03 09:55:47 -0400

    Prepare for epic4 project

---
+++ 00-scripts/utility_scripts/skim_stacks_workflow.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Remove any unneaded files from stacks_workflow
+# Launch from the base `stacks_workflow` directory
+
+rm -rf .git
+rm .gitignore
+rm -r 99-documentation
+rm -r 00-scripts/deprecated
+rm -r 00-scripts/test_stacks_workflow
+rm CHANGELOG LICENSE MANUAL.html README.md TODO.md
