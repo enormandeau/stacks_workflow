4bde485d08b15735edf7effd04348357497a5a70
enormandeau <eric.normandeau.qc@gmail.com>
2016-05-12 11:15:37 -0400

    Improve colosse scripts

--- 00-scripts/colosse_jobs/06_create_jobs_submission.sh
+++ 00-scripts/colosse_jobs/06_create_jobs_submission.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-
-
 # Create list of samples
-ls 04-all_samples/*f*q.gz|split -l 30 - list
+ls 04-all_samples/*f*q.gz | split -l 30 - list
 
-
-#change drectory
-
-for i in $(pwd); do sed -i "s#__PWD__#$i#g" 00-scripts/colosse_jobs/gsnap_stacks_template.sh; done
+#change drectory in gsnap template job
+i=$(pwd)
+sed "s#__PWD__#$i#g" 00-scripts/colosse_jobs/gsnap_stacks_template.sh > 00-scripts/colosse_jobs/gsnap_stacks.sh
 
 #create list of jobs
-for base in $(ls list*); do toEval="cat 00-scripts/colosse_jobs/gsnap_stacks_template.sh| sed 's/__LIST__/$base/g'"; eval $toEval > 00-scripts/colosse_jobs/GSNAP_$base.sh;done
+for base in $(ls list*)
+do
+    toEval="cat 00-scripts/colosse_jobs/gsnap_stacks.sh | sed 's/__LIST__/$base/g'"
+    eval $toEval > 00-scripts/colosse_jobs/GSNAP_$base.sh
+done
+
+exit
 
 #submit jobs
-for i in $(ls 00-scripts/colosse_jobs/GSNAP*sh); do msub $i; done
+for i in $(ls 00-scripts/colosse_jobs/GSNAP*sh)
+do
+    msub $i
+done
