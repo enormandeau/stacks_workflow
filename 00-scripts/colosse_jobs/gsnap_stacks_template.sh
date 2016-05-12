4a88d72fb844fcf987ef775b6c0cdcbac92974ad
enormandeau <eric.normandeau.qc@gmail.com>
2016-05-12 15:19:08 -0400

    Fix bug: .sam files now removed

--- 00-scripts/colosse_jobs/gsnap_stacks_template.sh
+++ 00-scripts/colosse_jobs/gsnap_stacks_template.sh
@@ -5,7 +5,7 @@
 #PBS -e gsnap__LIST__.err
 #PBS -l walltime=24:00:00
 #PBS -M your.address@here
-#PBS -m bea
+####PBS -m bea
 #PBS -l nodes=1:ppn=8
 #PBS -r n
 
@@ -17,6 +17,8 @@ cd $PBS_O_WORKDIR
 
 # Load gmap module
 module load apps/gmap/2015-12-31.v9
+module load compilers/gcc/4.8  apps/mugqic_pipeline/2.1.1
+module load mugqic/samtools/1.2
 
 #prepare the genome if not already done
 #gmap_build --dir=/rap/ihv-653-ab/00_ressources/01_genomes/Oniloticus/ /rap/ihv-653-ab/00_ressources/01_genomes/Oniloticus/oni_ref_Orenil1.1.fa -d gmap_oniloticuv1.1
@@ -43,5 +45,5 @@ do
 
     # Clean up
     echo "Removing $file"
-    rm $DATAFOLDER/"${file%.fq.gz}".sam
+    rm "${file%.fq.gz}".sam
 done
