a3fe80c8e4b391b2e829daf4f829c37654bed5f9
enormandeau <eric.normandeau.qc@gmail.com>
2016-05-12 11:37:12 -0400

    Formatting

--- 00-scripts/colosse_jobs/06_create_jobs_submission.sh
+++ 00-scripts/colosse_jobs/06_create_jobs_submission.sh
@@ -7,16 +7,14 @@ ls 04-all_samples/*f*q.gz | split -l 30 - list
 i=$(pwd)
 sed "s#__PWD__#$i#g" 00-scripts/colosse_jobs/gsnap_stacks_template.sh > 00-scripts/colosse_jobs/gsnap_stacks.sh
 
-#create list of jobs
+# Create list of jobs
 for base in $(ls list*)
 do
     toEval="cat 00-scripts/colosse_jobs/gsnap_stacks.sh | sed 's/__LIST__/$base/g'"
     eval $toEval > 00-scripts/colosse_jobs/GSNAP_$base.sh
 done
 
-exit
-
-#submit jobs
+# Submit jobs
 for i in $(ls 00-scripts/colosse_jobs/GSNAP*sh)
 do
     msub $i
