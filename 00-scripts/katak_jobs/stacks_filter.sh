7ce28b879c32873321b42e60c4f01f7147bd84a3
jleluyer <jeremy.le.luyer@gmail.com>
2016-05-17 12:44:46 -0400

    correct stacks_filter

--- 00-scripts/katak_jobs/stacks_filter.sh
+++ 00-scripts/katak_jobs/stacks_filter.sh
@@ -18,7 +18,7 @@ cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"
 cd $SGE_O_WORKDIR
 
 #global variables
-MAF_GLOBAL="- a0.01"	# global maf
+MAF_GLOBAL="-a 0.01"	# global maf
 MAF_POP="-A 0.05"		# population maf
 MIN_DEPTH="-m 7"		# min depth
 MIN_ALLELE_COVERAGE="-c 1"	# min allele
@@ -33,10 +33,10 @@ MAX_NB_SNPS="-s 10"		# max number of snps by loci
 MAX_FIS="-F 0.4"		#max fis
 #JOKER_FIS="-z 1"		#joker fis
 
-INPUT="./05-stacks/batch_1.vcf"
-OUTPUT="./05-stacks/batch_1_filtered.vcf"
+INPUT="-i filtered_no_paralog.vcf"
+OUTPUT="-o batch_1_filtered.vcf"
 
-./00-scripts/05_filter_vcf.py -q -i $INPUT $OUTPUT \
+./00-scripts/05_filter_vcf.py -q $INPUT $OUTPUT \
  		$MIN_PRES $MIN_ALLELE_COVERAGE $MIN_DEPTH $ALL_IMB $LOG_LIKELIHOOD \
 		--use_percent $MAF_GLOBAL $MAF_POP $HET $JOKER_HET \
 		$FIS $MAX_FIS $JOKER_POP $MAX_NB_SNPS $JOKER_FIS 2>&1 | tee 98-log_files/"$TIMESTAMP"_filter.log 
