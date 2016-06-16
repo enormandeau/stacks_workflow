31e9a925a443cf90f859f463cdaf840701c9b9f3
Eric Normandeau <eric.normandeau.qc@gmail.com>
2016-06-16 10:20:45 -0400

    Remove reporting from replace_email_addresses.sh

--- 00-scripts/utility_scripts/replace_email_addresses.sh
+++ 00-scripts/utility_scripts/replace_email_addresses.sh
@@ -4,8 +4,6 @@
 
 for script in 00-scripts/katak_jobs/*.sh
 do
-    echo "$script"
     grep "your.email@service.com" "$script"
-    echo "$YOUREMAIL"
-    perl -i.bak -sape 's/YOUREMAIL/$e/' -- -e="$YOUREMAIL" "$script"
+    perl -sape 's/YOUREMAIL/$e/' -- -e="$YOUREMAIL" "$script"
 done
