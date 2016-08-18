0644cbd5d9e774d019eeeeb18d3e422a629ffdff
Eric Normandeau <eric.normandeau.qc@gmail.com>
2016-08-18 11:34:42 -0400

    Save log of ustacks_iteration.sh instead of parallel script

--- 00-scripts/stacks_1a_ustacks_parallel.sh
+++ 00-scripts/stacks_1a_ustacks_parallel.sh
@@ -4,10 +4,10 @@ TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
 
 # Copy script as it was run
 SCRIPT=$0
-NAME=$(basename $0)
+NAME="00-scripts/ustacks_iteration.sh"
 LOG_FOLDER="10-log_files"
 
 cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"
 
 maxid=$(ls -1 04-all_samples/*.fq.gz | wc -l)
-seq $maxid | parallel -j 8 00-scripts/ustacks_iteration.sh
+seq $maxid | parallel -j 16 00-scripts/ustacks_iteration.sh
