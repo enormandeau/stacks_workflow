4fd7cdd7b1051050cabf98d2538be707c1e8fc6b
Eric Normandeau <eric.normandeau.qc@gmail.com>
2016-09-19 15:21:21 -0400

    Replace magic number by variable

--- 00-scripts/stacks_1a_ustacks_parallel.sh
+++ 00-scripts/stacks_1a_ustacks_parallel.sh
@@ -2,6 +2,9 @@
 # Launch ustacks to treat all the samples individually
 TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
 
+# Global variables
+NUM_CPUS=16
+
 # Copy script as it was run
 SCRIPT=$0
 NAME="ustacks_iteration.sh"
@@ -10,4 +13,4 @@ LOG_FOLDER="10-log_files"
 cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"
 
 maxid=$(ls -1 04-all_samples/*.fq.gz | wc -l)
-seq $maxid | parallel -j 16 00-scripts/ustacks_iteration.sh
+seq $maxid | parallel -j $NUM_CPUS 00-scripts/ustacks_iteration.sh
