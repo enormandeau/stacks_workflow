ab726781b9c81f97fe509132e29d15f9af9c8033
Eric Normandeau <eric.normandeau.qc@gmail.com>
2017-05-05 11:59:12 -0400

    Add script to keep one SNP with the best MAF

---
+++ 00-scripts/utility_scripts/extract_snp_with_max_maf.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+"""Keep only the first SNP per locus in vcf file
+
+Usage:
+    ./extract_first_snp.py input_vcf output_vcf
+"""
+
+# Modules
+import sys
+
+# Functions
+
+# Main
+if __name__ == '__main__':
+    # Parse user input
+    try:
+        input_vcf = sys.argv[1]
+        output_vcf = sys.argv[2]
+    except:
+        print __doc__
+        sys.exit(1)
+
+    # Open file handles
+    infile = open(input_vcf)
+    outfile = open(output_vcf, "w")
+
+    # Extract header and SNPs
+    extracted_snps = dict()
+
+    line_counter = 0
+    for line in infile:
+        line_counter += 1
+
+        # Header
+        if line.startswith("#"):
+            outfile.write(line)
+
+        # Loci
+        else:
+            snp_id = line.strip().split()[2]
+            maf = float(line.strip().split()[7].split(";")[1].split("=")[1])
+
+            # Correct for snp id format
+            if "_" in snp_id:
+                snp_id = snp_id.split("_")[0]
+
+            # Decide if we keep the snp
+            if snp_id not in extracted_snps:
+                extracted_snps[snp_id] = (line_counter, maf, line)
+                #print "new", snp_id, maf
+                #print maf
+
+            else:
+                if maf > extracted_snps[snp_id][1]:
+                    #print "better", snp_id, ">>", maf
+                    extracted_snps[snp_id] = (line_counter, maf, line)
+
+    snps = sorted(extracted_snps.values())
+    for snp in snps:
+        line = snp[2]
+        outfile.write(line)
+
+    # Close file handles
+    infile.close()
+    outfile.close()
