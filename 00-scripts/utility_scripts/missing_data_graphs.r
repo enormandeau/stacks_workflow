83ec8206392bffe7126904caa3895c7dc94ce40b
Eric Normandeau <eric.normandeau.qc@gmail.com>
2017-07-24 10:44:18 -0400

    Improve missing data figure

--- 00-scripts/utility_scripts/missing_data_graphs.r
+++ 00-scripts/utility_scripts/missing_data_graphs.r
@@ -11,17 +11,27 @@ data = read.table(paste(folder, "missing_data.tsv", sep="/"), header=T)
 # Produce graph by population
 png(graph_name, width=1000, height=500)
 
+    # Increase bottom margin for population names
+    par(mar=c(8,5,4,2))
+
     # Create basic boxplot
     boxplot(data$ProportionMissing ~ data$Population,
             main="Proportion of missing genotypes by population",
-            xlab="Population",
+            xlab="",
             ylab="Proportion of missing genotypes",
             ylim=c(0, 1),
+            yaxp=c(0, 1, 10),
             col="lightgrey",
-            outcol="transparent")
+            outcol="transparent",
+            las=2)
 
     # Add dotted lines at every 5%
-    for (i in seq(0, 1, 0.05)) {
+    for (i in seq(0.05, 1, 0.1)) {
+        abline(h=i, lty=3, col="lightgrey")
+    }
+
+    # Add darker lines every 10%
+    for (i in seq(0, 1, 0.1)) {
         abline(h=i, lty=2, col="lightgrey")
     }
 
