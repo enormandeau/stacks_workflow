e825cca6d07b226e8c66690d931bed05cba99802
Eric Normandeau <eric.normandeau.qc@gmail.com>
2018-05-09 08:51:45 -0400

    Add 00-scripts/utility_scripts/vcf_summarize_duplicates.py

--- 00-scripts/utility_scripts/vcf_summarize_duplicates.py
+++ 00-scripts/utility_scripts/vcf_summarize_duplicates.py
@@ -2,7 +2,7 @@
 """Summarize reproducibility of RADSeq using duplicates
 
 Usage:
-    <program> input_vcf separator_character output_summary
+    <program> input_vcf separator_character output_filename
 """
 
 # Module
@@ -13,7 +13,7 @@ import sys
 try:
     input_vcf = sys.argv[1]
     separator_character = sys.argv[2]
-    output_summary = sys.argv[3]
+    output_filename = sys.argv[3]
 except:
     print(__doc__)
     sys.exit(1)
@@ -55,7 +55,7 @@ duplicate_cases = ["both_missing",
         "diff_homo"]
 
 with open(input_vcf) as infile:
-    with open(output_summary, "w") as outfile:
+    with open(output_filename, "w") as outfile:
         header = "Contig\tcontig_pos\tlocus_id\tsnp_pos\tallele1\tallele2\tnum_samples\tmaf\tsample1\tgeno1\tcov1\ta1_cov1\ta2_cov1\tsample2\tgeno2\tcov2\ta1_cov2\ta2_cov2\n"
         outfile.write(header)
 
@@ -87,37 +87,39 @@ with open(input_vcf) as infile:
                 geno2 = geno_dictionary[geno2]
                 cov2_a1, cov2_a2 = allele_cov2.split(",")
 
-                min_cov = min([cov1, cov2])
+                min_cov = str(min([int(cov1), int(cov2)]))
 
                 if geno1 == geno2 == -9:
                     duplicates_dict[stub]["both_missing"] += 1
+                    duplicates_per_coverage_dict[stub]["both_missing"][min_cov] += 1
 
                 elif geno1 == -9 or geno2 == -9:
                     duplicates_dict[stub]["one_missing"] += 1
+                    duplicates_per_coverage_dict[stub]["one_missing"][min_cov] += 1
 
                 elif geno1 == geno2 == 0:
                     duplicates_dict[stub]["both_homo_frequent"] += 1
-                    duplicates_per_coverage_dict[stub][min_cov]["both_homo_frequent"] += 1
+                    duplicates_per_coverage_dict[stub]["both_homo_frequent"][min_cov] += 1
 
                 elif geno1 == geno2 == 1:
                     duplicates_dict[stub]["both_hetero"] += 1
-                    duplicates_per_coverage_dict[stub][min_cov]["both_hetero"] += 1
+                    duplicates_per_coverage_dict[stub]["both_hetero"][min_cov] += 1
 
                 elif geno1 == geno2 == 2:
                     duplicates_dict[stub]["both_homo_rare"] += 1
-                    duplicates_per_coverage_dict[stub][min_cov]["both_homo_rare"] += 1
+                    duplicates_per_coverage_dict[stub]["both_homo_rare"][min_cov] += 1
 
                 elif geno1 == 1 and geno2 in [0, 2]:
                     duplicates_dict[stub]["hetero_homo"] += 1
-                    duplicates_per_coverage_dict[stub][min_cov]["hetero_homo"] += 1
+                    duplicates_per_coverage_dict[stub]["hetero_homo"][min_cov] += 1
 
                 elif geno1 in [0, 2] and geno2 == 1:
                     duplicates_dict[stub]["hetero_homo"] += 1
-                    duplicates_per_coverage_dict[stub][min_cov]["hetero_homo"] += 1
+                    duplicates_per_coverage_dict[stub]["hetero_homo"][min_cov] += 1
 
                 elif (geno1 == 0 and geno2 == 2) or (geno1 == 2 and geno2 == 0):
                     duplicates_dict[stub]["diff_homo"] += 1
-                    duplicates_per_coverage_dict[stub][min_cov]["diff_homo"] += 1
+                    duplicates_per_coverage_dict[stub]["diff_homo"][min_cov] += 1
 
                 else:
                     print("You missed a case, dumass!")
@@ -132,10 +134,21 @@ with open(input_vcf) as infile:
 
                 outfile.write(report)
 
-for sample in pairs:
-    for case in duplicate_cases:
-        for cov in range(4, 11):
+# Summary
+with open(output_filename + "_summary.tsv", "w") as outfile:
+    for sample in pairs:
+        for case in duplicate_cases:
+            outfile.write(
+                    "\t".join([sample, case,
+                        str(sum(duplicates_per_coverage_dict[sample][case].values()))]) +
+                    "\n")
 
-            print(sample, case, duplicates_per_coverage_dict[sample][cov][case])
-
-    print("--")
+# By coverage
+with open(output_filename + "_summary_by_coverage.tsv", "w") as outfile:
+    for sample in pairs:
+        for case in duplicate_cases:
+            for cov in range(4, 101):
+                outfile.write(
+                        "\t".join([sample, case, str(cov),
+                            str(duplicates_per_coverage_dict[sample][case][str(cov)])]) +
+                        "\n")
