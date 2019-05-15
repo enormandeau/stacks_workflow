95d9c04faeefd9bd21cb8f664075164c456303c4
Eric Normandeau <eric.normandeau.qc@gmail.com>
2019-05-15 14:11:30 -0400

    Filter SNPs with few samples with rare allele

--- 00-scripts/09_explore_overmerged_loci_02.R
+++ 00-scripts/09_explore_overmerged_loci_02.R
@@ -14,7 +14,7 @@ duplicated =    "#FF000022" # red
 diverged =      "#0000FF22" # blue
 lowconfidence = "#DD00AA22" # purple
 highcov =       "#00AA0022" # green
-#test =          "#FFAA0022" # orange
+#orange =          "#FFAA0022" # orange
 
 # All loci marked singleton before filters
 d$Color = singleton
@@ -30,12 +30,14 @@ d$Color[d$Fis + d$MedRatio / 3 < 0.11] = duplicated
 # Very low Fis = diverged
 d$Color[d$Fis < -0.6] = diverged
 
-# Very high Fis = low coverage and low confidence
-#d$Color[d$Fis - d$PropHet / 4 > 0.45] = lowconfidence
+# High Fis
 d$Color[d$Fis > 0.49] = lowconfidence
 
 # Loci with high coverage
 d$Color[d$MedCovHom > 40 | d$MedCovHet > 40] = highcov
+
+# Too few samples with rare allele
+d$Color[data$NumHet + data$NumRare <= 2] = lowconfidence
 
 # Extract bad loci infos
 bad_snps = d$Color != singleton
@@ -45,7 +47,7 @@ bad_loci = unique(gsub("_.*", "", data$ID[bad_snps]))
 # Categorize SNPs to filter loci with next script
 data$Category = "singleton"
 data$Category[d$Color == duplicated] = "duplicated"
-#data$Category[d$Color == test] = "duplicated"
+#data$Category[d$Color == orange] = "lowconfidence"
 data$Category[d$Color == diverged] = "diverged"
 data$Category[d$Color == lowconfidence] = "lowconfidence"
 data$Category[d$Color == highcov] = "highcov"
@@ -70,7 +72,7 @@ png(paste0(input_file, "_2.png"), width=1200, height=950)
     plot(d$PropHet, d$MedRatio, pch=19, cex=1.5, col=d$Color, xlim=c(0, 1), ylim=c(0, 0.8))
 invisible(dev.off())
 
-single = d[data$Category == "singleton", ]# | data$Category == "highcov" | data$Category == "test", ]
+single = d[data$Category == "singleton", ]
 png(paste0(input_file, "_3.png"), width=1200, height=950)
     plot(single$PropHet,
          single$MedRatio,
