164fcea1440a98708831f43cc00702f8deb4f01c
Eric Normandeau <eric.normandeau.qc@gmail.com>
2019-07-05 11:53:40 -0400

    Add 06_filter_samples_with_list.py

---
+++ 00-scripts/06_filter_samples_with_list.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""Filtering VCF files output by STACKS1 or STACKS2 to remove samples
+
+Usage:
+    <program> input_vcf filtering_type info_file output_vcf
+
+Where:
+    input_vcf is the name of the VCF file to filter
+    filtering_type is either "wanted" or "unwanted"
+    info_file contains wanted or blacklisted sample names
+    output_vcf is the name of the filtered VCF
+
+Warning:
+    The allele frequency values are not recomputed after filtering the samples
+"""
+
+# Import
+import sys
+
+# Parse user input
+try:
+    input_vcf = sys.argv[1]
+    filtering_type = sys.argv[2]
+    info_file = sys.argv[3]
+    output_vcf = sys.argv[4]
+except:
+    print(__doc__)
+    sys.exit(1)
+
+if not filtering_type in ["wanted", "unwanted"]:
+    print("ERROR: filtering_type must be 'wanted' or 'unwanted'")
+    sys.exit(2)
+
+# Get wanted or blacklisted samples
+listed_samples = set([x.strip() for x in open(info_file).readlines()])
+
+# Filter
+with open(input_vcf) as infile:
+    with open(output_vcf, "w") as outfile:
+        for line in infile:
+            l = line.strip().split("\t")
+
+            if line.startswith("##"):
+                outfile.write(line)
+                continue
+
+            if line.startswith("#CHROM"):
+
+                samples = l[9:]
+                sample_ids = set()
+
+                for i, s in enumerate(samples):
+                    if s in listed_samples:
+                        sample_ids.add(i)
+
+                if filtering_type == "wanted":
+                    wanted_ids = [x for x in range(len(samples)) if x in sample_ids]
+
+                elif filtering_type == "unwanted":
+                    wanted_ids = [x for x in range(len(samples)) if x not in sample_ids]
+
+            infos = l[:9]
+            genotypes = l[9:]
+            genotypes = [genotypes[i] for i in wanted_ids]
+            filtered_line = "\t".join(infos + genotypes) + "\n"
+            outfile.write(filtered_line)
