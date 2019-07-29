42283c5c1781ec82d118edde89ebcc511e277492
Eric Normandeau <eric.normandeau.qc@gmail.com>
2019-07-29 14:19:22 -0400

    Add denovo STACKS scripts

---
+++ 00-scripts/stacks2_cstacks.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Launch populations
+TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
+
+# Copy script as it was run
+SCRIPT=$0
+NAME=$(basename $0)
+SAMPLE_FOLDER="04-all_samples"
+STACKS_FOLDER="05-stacks"
+LOG_FOLDER="10-log_files"
+INFO_FILES_FOLDER="01-info_files"
+POP_MAP="population_map.txt"
+
+# Number of CPUs
+NUM_CPU="4"
+
+
---
+++ 00-scripts/stacks2_ustacks.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Launch populations
+TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
+
+# Copy script as it was run
+SCRIPT=$0
+NAME=$(basename $0)
+SAMPLE_FOLDER="04-all_samples"
+STACKS_FOLDER="05-stacks"
+LOG_FOLDER="10-log_files"
+INFO_FILES_FOLDER="01-info_files"
+POP_MAP="population_map.txt"
+
+# Number of CPUs
+# TODO use gnu parallel
+NUM_CPU="1"
+
+id=1
+ls -1 "$SAMPLE_FOLDER"/*.gz |
+while read file
+do
+    name=$(basename "$file") 
+    echo
+    echo "#######################################"
+    echo "  Treating indivudual $id: ${name%.fq.gz}"
+    echo "#######################################"
+    echo
+
+    ustacks -f "$SAMPLE_FOLDER"/"$name" -o "$STACKS_FOLDER" -i $id -p 1 \
+        -m 4 -M 3 -N 5 -H --deleverage
+    let "id+=1"
+
+done
