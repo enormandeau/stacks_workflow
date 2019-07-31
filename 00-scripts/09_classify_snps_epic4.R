5abd800e6bdeaf70315308d324ee7a0cd3c8a28e
Eric Normandeau <eric.normandeau.qc@gmail.com>
2019-07-31 14:04:25 -0400

    Improve SNP categorization for EPIC4

--- 00-scripts/09_classify_snps_epic4.R
+++ 00-scripts/09_classify_snps_epic4.R
@@ -7,40 +7,50 @@ output_file = paste0(input_file, ".categorized")
 
 # Load data
 data = read.table(input_file, header=T, stringsAsFactors=F)
-d = data[,c("MedRatio", "PropHet", "PropHomRare", "Fis", "MedCovHet", "MedCovHom")]
+#d = data[,c("MedRatio", "PropHet", "PropHomRare", "Fis", "MedCovHet", "MedCovHom")]
+d = data[,c("AvgRatio", "PropHet", "PropHomRare", "Fis", "MedCovHet", "MedCovHom")]
+names(d) = c("MedRatio", "PropHet", "PropHomRare", "Fis", "MedCovHet", "MedCovHom")
 
 singleton =     "#00000011" # black
 duplicated =    "#FF000022" # red
 diverged =      "#0000FF22" # blue
-lowconf =       "#DD00AA22" # purple
-highcov =       "#00AA0022" # green
-mas =           "#FFAA0022" # orange
+lowconf =       "#DD00DD44" # purple
+highcov =       "#00AA0088" # green
+mas =           "#FFAA0044" # orange
 
 # All loci marked singleton before filters
 d$Color = singleton
 
-# MedRatio is high/low and at least one rare allele homozygote
-d$Color[d$MedRatio < 0.30] = lowconf # & d$PropHomRare > 0.00] = lowconf
-d$Color[d$MedRatio > 0.70] = lowconf # & d$PropHomRare > 0.00] = lowconf
-
 # Fis is too negative = duplicated
-d$Color[d$Fis < -0.3] = duplicated
-d$Color[d$Fis + d$MedRatio < 0.33] = duplicated
+d$Color[d$Fis < -0.30] = duplicated
+d$Color[d$MedRatio > 0.22 & d$Fis < -0.45] = duplicated
+d$Color[d$MedRatio > 0.22 & d$Fis + d$MedRatio < 0.10] = duplicated
+d$Color[d$MedRatio > 0.22 & d$Fis + d$MedRatio * 3 < 1.00] = duplicated
+d$Color[d$MedRatio > 0.22 & d$Fis + d$MedRatio * 8 < 2.85] = duplicated
+
+# Very low Fis = diverged
+d$Color[d$Fis < -0.1 & d$Fis + d$MedRatio < -0.175] = diverged
+d$Color[d$Fis < -0.1 & d$Fis + d$MedRatio * 3 < 0.54] = diverged
+d$Color[d$Fis < -0.1 & d$Fis + d$MedRatio * 8 < 1.70] = diverged
+d$Color[d$Fis < -0.1 & d$MedRatio < 0.22] = diverged
+
+# Salvage back some singletons
+d$Color[d$Fis > 0.0 & d$MedRatio + d$Fis * 2 > 0.35 & d$MedRatio < 0.40] = singleton
+
+# MedRatio is high/low and Fis is too positive
+d$Color[d$Fis > 0.0 & d$MedRatio + d$Fis * 2 > 0.35 & d$MedRatio < 0.28] = lowconf
+d$Color[d$Fis > 0.0 & d$MedRatio + d$Fis * 2 > 0.35 & d$MedRatio > 0.7] = lowconf
+d$Color[d$Fis > -0.1 & d$MedRatio < 0.22] = lowconf
+d$Color[d$Fis > -0.1 & d$MedRatio > 0.70] = lowconf
+
+# Loci with high coverage
+d$Color[d$MedCovHom > 100 | d$MedCovHet > 100] = highcov
 
 # Very low Fis = diverged
 d$Color[d$Fis < -0.65] = diverged
 
-# High Fis
-d$Color[d$Fis > 0.7] = lowconf
-
-# MedRatio is high/low and at least one rare allele homozygote
-d$Color[d$MedRatio < 0.20] = lowconf # & d$PropHomRare > 0.00] = lowconf
-
-# Loci with high coverage
-d$Color[d$MedCovHom > 80 | d$MedCovHet > 80] = highcov
-
 # Too few samples with rare allele
-d$Color[data$NumHet + data$NumRare < 5] = mas
+d$Color[data$NumHet + data$NumRare < 8] = mas
 
 # Extract bad loci infos
 bad_snps = d$Color != singleton
@@ -59,13 +69,13 @@ write.table(data[,c("Scaffold", "Position", "ID", "Category")],
             output_file, sep="\t", quote=F, row.names=F)
 
 # Report number of SNPs per category
-report = table(data$Category)
 cat("SNPs")
-print(report)
+print(table(data$Category))
 
 # Plots
+#png(paste0(input_file, "_1.png"), width=1200, height=950)
 png(paste0(input_file, "_1.png"), width=1200, height=950)
-    plot(d[,1:4], pch=16, cex=1, col=d$Color)
+    plot(d[,1:4], pch=16, cex=0.8, col=d$Color)
 invisible(dev.off())
 
 png(paste0(input_file, "_2.png"), width=1200, height=950)
