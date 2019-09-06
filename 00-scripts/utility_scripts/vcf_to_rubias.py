7cf8f4ce6b77a7620b89104a66ce02a47c5b1bb6
Eric Normandeau <eric.normandeau.qc@gmail.com>
2019-09-06 15:14:43 -0400

    Finished vcf_to_rubias.py

--- 00-scripts/utility_scripts/vcf_to_rubias.py
+++ 00-scripts/utility_scripts/vcf_to_rubias.py
@@ -19,6 +19,33 @@ except:
     sys.exit(1)
 
 # Read VCF and collect information
-print("hello")
+samples_info = []
+loci_names = ["Sample"]
+
+with open(input_vcf) as infile:
+    for line in infile:
+        l = line.strip().split("\t")
+        locus = "_".join(l[:3])
+
+        if line.startswith("##"):
+            continue
+
+        elif line.startswith("#CHROM"):
+            for i in range(9, len(l)):
+                samples_info.append([l[i]])
+
+        else:
+            loci_names.append(locus)
+            loci_names.append(locus)
+            for i in range(9, len(l)):
+                genotype_info = l[i].split(":")[0]
+                genotype_info = genotype_info.split("/")
+                genotype_info = [x if x != "." else "NA" for x in genotype_info]
+                samples_info[i-9] += genotype_info
 
 # Write rubias file one line (or sample) at a time
+with open(output_rubias, "w") as outfile:
+    outfile.write("\t".join(loci_names) + "\n")
+
+    for sample in samples_info:
+        outfile.write("\t".join(sample) + "\n")
