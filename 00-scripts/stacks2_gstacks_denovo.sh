0f76c0e76d5b5dc65d15a9ebd327f81ea2065b3e
Eric Normandeau <eric.normandeau.qc@gmail.com>
2021-04-07 09:01:08 -0400

    Bugfix: Point gstacks to proper folder

--- 00-scripts/stacks2_gstacks_denovo.sh
+++ 00-scripts/stacks2_gstacks_denovo.sh
@@ -5,7 +5,7 @@ TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
 # Copy script as it was run
 SCRIPT=$0
 NAME=$(basename $0)
-SAMPLE_FOLDER="04-all_samples"
+SAMPLE_FOLDER="05-stacks"
 STACKS_FOLDER="05-stacks"
 LOG_FOLDER="10-log_files"
 INFO_FILES_FOLDER="01-info_files"
