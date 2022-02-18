5f60634a79b58c1f20d4d9ee4ebccf96d708f93e
Eric Normandeau <eric.normandeau.qc@gmail.com>
2022-02-18 15:11:59 -0500

    Update 09_classify_snps_melanieG.R

--- 00-scripts/classify_snps/09_classify_snps_melanieG.R
+++ 00-scripts/classify_snps/09_classify_snps_melanieG.R
@@ -20,11 +20,11 @@ mas =           "#FFAA0022" # orange
 d$Color = singleton
 
 # MedRatio is high/low and at least one rare allele homozygote
-d$Color[d$MedRatio < 0.35] = lowconf # & d$PropHomRare > 0.00] = lowconf
-d$Color[d$MedRatio > 0.65] = lowconf # & d$PropHomRare > 0.00] = lowconf
+d$Color[d$MedRatio < 0.30] = lowconf # & d$PropHomRare > 0.00] = lowconf
+d$Color[d$MedRatio > 0.70] = lowconf # & d$PropHomRare > 0.00] = lowconf
 
 # Fis is too negative = duplicated
-d$Color[d$Fis < -0.4] = duplicated
+d$Color[d$Fis < -0.3] = duplicated
 d$Color[d$Fis + d$MedRatio < 0.08] = duplicated
 d$Color[d$Fis + d$MedRatio * 3 < 0.78] = duplicated
 d$Color[d$Fis + d$MedRatio * 8 < 2.3] = duplicated
@@ -36,7 +36,7 @@ d$Color[d$Fis + d$MedRatio * 3 < 0.20] = diverged
 d$Color[d$Fis + d$MedRatio * 8 < 1.5] = diverged
 
 # High Fis
-d$Color[d$Fis > 0.6] = lowconf
+d$Color[d$Fis > 0.7] = lowconf
 
 # Loci with high coverage
 d$Color[d$MedCovHom > 60 | d$MedCovHet > 60] = highcov
