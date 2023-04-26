758178209e9da2e7fe2d1701f7a008fe0200191a
Eric Normandeau <eric.normandeau.qc@gmail.com>
2023-04-26 10:44:27 -0400

    Add plot_pairwise_fst.R

---
+++ 00-scripts/utility_scripts/plot_pairwise_fst.R
@@ -0,0 +1,21 @@
+#!/usr/bin/env Rscript
+
+# Modules
+library(tidyverse)
+
+# Parse user input
+args = commandArgs(trailingOnly=TRUE)
+input_file = args[1]
+
+# Read data
+data = rev(read.table(input_file, header=T, sep="\t"))
+
+# Create heatmap
+ggp <- ggplot(data, aes(Pop1, Pop2)) +    # Create default ggplot2 heatmap
+    geom_tile(aes(fill = Fst)) +
+    scale_y_discrete() +
+    geom_text(aes(label = Fst)) +
+    scale_fill_gradient(low = "#AACCEE", high = "#116699")
+
+# Write to file
+ggsave(paste0(tools::file_path_sans_ext(input_file), ".pdf"), ggp)
