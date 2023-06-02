dfd39f72aac4a562f96d75c965286439cd5947ec
Eric Normandeau <eric.normandeau.qc@gmail.com>
2023-06-02 08:53:22 -0400

    Improve plot_pca.R

--- 00-scripts/utility_scripts/plot_pca.R
+++ 00-scripts/utility_scripts/plot_pca.R
@@ -1,15 +1,16 @@
 #!/usr/bin/env Rscript
 
 # Usage:
-#     <program> prefix popmap npc_to_plot output
-#     prefix: prefix to  012 files; ex : test to read test.012, test.012.indv 7 test.012.pos
-#           (prepared with vcftools -012 option)
-#     popmap: 2tab delimited column: first = ind name, second = pop. no header
-#     npc_to_plot: number of PCs to plot
+#     <program> prefix popmap numPC output
+#     prefix: prefix of 012 files (ex : test for test.012, test.012.indv 7 test.012.pos)
+#           Prepared with vcftools -012
+#     popmap: 2+ tab delimited columns WITH HEADER: first = Sample, second = GROUP
+#           There can be as many columns as wanted after the first two
+#     numPC: number of PCs to plot
 #     output: Name of output file
-# 
+#
 # Note:
-#     FILE SHOULD BE IMPUTED, will not be performed in this script Use 02_impute.py to perform simple imputation
+#     012 file must be created from imputed VCF
 
 # Code adapted from Florent Sylvestre, 2023-04-25
 
@@ -22,32 +23,40 @@ library(ggplot2)
 if(length(commandArgs(trailingOnly = T)) == 4) {
 
     ##read input file
-    dat <- as.data.frame(fread(paste0(commandArgs(trailingOnly = T)[1], ".012"))[, -1])
-    ind = as.data.frame(fread(paste0(commandArgs(trailingOnly = T)[1], ".012.indv"), header = F))
-    popmap <- read.table(commandArgs(trailingOnly = T)[2])
-    NPC_to_plot <- as.numeric(commandArgs(trailingOnly = T)[3])
-    output <- commandArgs(trailingOnly = T)[4]
+    dat <- as.data.frame(fread(paste0(commandArgs(trailingOnly=T)[1], ".012"))[, -1])
+    ind = as.data.frame(fread(paste0(commandArgs(trailingOnly=T)[1], ".012.indv"), header=F))
+    popmap <- read.table(commandArgs(trailingOnly=T)[2], header=T)
+    numPC <- as.numeric(commandArgs(trailingOnly=T)[3])
+    output <- commandArgs(trailingOnly=T)[4]
 
 } else {
 
     cat("
 Usage:
-    <program> prefix popmap npc_to_plot output
-    prefix : prefix to  012 files; ex : test to read test.012, test.012.indv 7 test.012.pos
-    popmap :: 2tab delimited column: first = ind name, second = pop. no header
+    <program> prefix popmap numPC output
+
+    prefix: prefix of 012 files (ex : test for test.012, test.012.indv 7 test.012.pos)
+        Prepared with vcftools -012
+    popmap: 2+ tab delimited columns WITH HEADER: first = Sample, second = GROUP
+        There can be as many columns as wanted after the first two
+    numPC: number of PCs to plot
+    output: Name of output file
 
 Note:
-    FILE SHOULD BE IMPUTED, will not be performed in this script Use 02_impute.py to perform simple imputation
+    012 file must be created from imputed VCF
+        ")
 
-")
-
-quit()
+        quit()
 
 }
 
 ##running pca
-ind <- merge(ind, popmap, sort=F)
+ind <- merge(ind, popmap, by.x=1, by.y="Sample", sort=F)
 pca <- prcomp(dat, scale=F)
+
+names(ind)[1] = "Sample"
+write.table(cbind(ind, pca$x[, 1:20]), paste0(output, "_used_samples_with_pca.tsv"), quote=F, row.names=F, sep="\t")
+#write.table(pca$x, "pc_loadings.tsv", quote=F, row.names=F, sep="\t")
 
 ###plots
 PCA_plot <- function(pca, Group, pcvector=c(1, 2)) {
@@ -55,35 +64,35 @@ PCA_plot <- function(pca, Group, pcvector=c(1, 2)) {
     sd <- pca$sdev/sum(pca$sdev) * 100
 
     pca$x %>%
-    as.data.frame() %>%
+        as.data.frame() %>%
 
-    ggplot(aes_string(x = paste0("PC", pcvector[1]),
-                      y = paste0("PC", pcvector[2]))) +
+        ggplot(aes_string(x = paste0("PC", pcvector[1]),
+                          y = paste0("PC", pcvector[2]))) +
 
-    geom_point(aes(col = Group)) +
-    #geom_point(aes(col = Group, shape = Group %% 6)) +
+        geom_point(aes(col = Group)) +
+        #geom_point(aes(col = Group, shape = Group %% 6)) +
 
-    stat_ellipse(aes(col = as.factor(Group))) +
+        stat_ellipse(aes(col = as.factor(Group))) +
 
-    theme_bw() +
+        theme_bw() +
 
-    labs(x = paste0("PC", pcvector[1], "   (", trunc(sd[pcvector[1]] * 100)/100, "%)"),
-         y = paste0("PC", pcvector[2], "   (", trunc(sd[pcvector[2]] * 100)/100, "%)"),
-         col = "Group")
+        labs(x = paste0("PC", pcvector[1], "   (", trunc(sd[pcvector[1]] * 100)/100, "%)"),
+             y = paste0("PC", pcvector[2], "   (", trunc(sd[pcvector[2]] * 100)/100, "%)"),
+             col = "Group")
 
 }
 
 List_plot <- list()
 
-for( i in seq(1: NPC_to_plot)[c(T, F)]) {
+for( i in seq(1: numPC)[c(T, F)]) {
 
-    if(i != NPC_to_plot) {
+    if(i != numPC) {
 
-        List_plot[[paste(i)]] <- PCA_plot(pca, ind$V2, c(i, i+1))
+        List_plot[[paste(i)]] <- PCA_plot(pca, ind$Group, c(i, i+1))
 
     } else {
 
-        List_plot[[paste(i)]] <- PCA_plot(pca, ind$V2, c(i, i-1))
+        List_plot[[paste(i)]] <- PCA_plot(pca, ind$Group, c(i, i-1))
     }
 
     ggsave(paste0(output, "_", (i+1)/2, ".png"),
@@ -99,9 +108,4 @@ for( i in seq(1: NPC_to_plot)[c(T, F)]) {
            height=8,
            width=10,
            units="in")
-
-    #ggsave(paste0(output, "_", i, ".png"), plot = List_plot[[paste(i)]] +
-    #   scale_color_manual(values = c("red", "blue")), device = "png")
-    #ggsave(paste0(output, "_", i, ".pdf"), plot = List_plot[[paste(i)]] +
-    #   scale_color_manual(values = c("red", "blue")), device = "pdf")
 }
