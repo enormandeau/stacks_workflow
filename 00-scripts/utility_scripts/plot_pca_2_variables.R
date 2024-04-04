1485060e8c26ae4c0b1c19e5994c8759220f4cac
Eric Normandeau <eric.normandeau.qc@gmail.com>
2024-04-04 10:06:44 -0400

    Improve PCA plots

--- 00-scripts/utility_scripts/plot_pca_2_variables.R
+++ 00-scripts/utility_scripts/plot_pca_2_variables.R
@@ -1,12 +1,12 @@
 #!/usr/bin/env Rscript
 
 # Usage:
-#     <program> prefix popmap numPC variable output
+#     <program> prefix popmap numPC variable1 variable2 output
 #     prefix: prefix of 012 files (ex : test for test.012, test.012.indv 7 test.012.pos)
 #           Prepared with vcftools -012
 #     popmap: 2+ tab delimited columns WITH HEADER: first = Sample
-#           There can be as many columns as wanted after the first two but one of them
-#           must be the `variable` of interest
+#           There can be as many columns as wanted after the first two but two of them
+#           must be the `variable1` and `variable2` of interest
 #     numPC: number of PCs to plot
 #     output: Name of output file
 #
@@ -21,17 +21,18 @@ library(magrittr)
 library(ggplot2)
 
 # Parse user input
-if(length(commandArgs(trailingOnly = T)) == 5) {
+if(length(commandArgs(trailingOnly = T)) == 6) {
 
     ##read input file
     dat <- as.data.frame(fread(paste0(commandArgs(trailingOnly=T)[1], ".012"))[, -1])
     ind = as.data.frame(fread(paste0(commandArgs(trailingOnly=T)[1], ".012.indv"), header=F))
     popmap <- read.table(commandArgs(trailingOnly=T)[2], header=T)
     numPC <- as.numeric(commandArgs(trailingOnly=T)[3])
-    variable <- as.character(commandArgs(trailingOnly=T)[4])
-    output <- commandArgs(trailingOnly=T)[5]
+    variable1 <- as.character(commandArgs(trailingOnly=T)[4])
+    variable2 <- as.character(commandArgs(trailingOnly=T)[5])
+    output <- commandArgs(trailingOnly=T)[6]
 
-    cat(paste0("Producing figure for: ", variable, "\n"))
+    cat(paste0("Producing figure for: ", variable1, " and ", variable2, "\n"))
 
 } else {
 
@@ -42,14 +43,15 @@ Usage:
     prefix: prefix of 012 files (ex : test for test.012, test.012.indv 7 test.012.pos)
         Prepared with vcftools -012
     popmap: 2+ tab delimited columns WITH HEADER: first = Sample
-        There can be as many columns as wanted after the first two but one of them
-        must be the `variable` of interest
+           There can be as many columns as wanted after the first two but two of them
+           must be the `variable1` and `variable2` of interest
     numPC: number of PCs to plot
     output: Name of output file
 
 Note:
     012 file must be created from imputed VCF
-        ")
+
+")
 
         quit()
 
@@ -57,15 +59,17 @@ Note:
 
 ##running pca
 ind <- merge(ind, popmap, by.x=1, by.y="Sample", sort=F)
-ind[, variable] = as.character(ind[, variable])
+ind[, variable1] = as.character(ind[, variable1])
+ind[, variable2] = as.factor(ind[, variable2])
+
 pca <- prcomp(dat, scale=F)
 
 names(ind)[1] = "Sample"
 write.table(cbind(ind, pca$x[, 1:20]), paste0(output, "_used_samples_with_pca.tsv"), quote=F, row.names=F, sep="\t")
-#write.table(pca$x, "pc_loadings.tsv", quote=F, row.names=F, sep="\t")
+write.table(pca$x, "pc_loadings.tsv", quote=F, row.names=F, sep="\t")
 
 ###plots
-PCA_plot <- function(pca, ind, v1, pcvector=c(1, 2)) {
+PCA_plot <- function(pca, ind, v1, v2, pcvector=c(1, 2)) {
 
     sd <- pca$sdev/sum(pca$sdev) * 100
 
@@ -75,13 +79,14 @@ PCA_plot <- function(pca, ind, v1, pcvector=c(1, 2)) {
         ggplot(aes_string(x = paste0("PC", pcvector[1]),
                           y = paste0("PC", pcvector[2]))) +
 
-        geom_point(aes(col = ind[, v1], size=3, alpha=0.6, fill=NA)) +
+        geom_point(aes(col = ind[, v1], shape = ind[, v2]), size=3, alpha=0.6, fill=NA) +
 
         theme_bw() +
 
         labs(x = paste0("PC", pcvector[1], "   (", trunc(sd[pcvector[1]] * 100)/100, "%)"),
              y = paste0("PC", pcvector[2], "   (", trunc(sd[pcvector[2]] * 100)/100, "%)"),
-             col = v1)
+             col = v1,
+             shape = v2)
 }
 
 List_plot <- list()
@@ -90,11 +95,11 @@ for( i in seq(1: numPC)[c(T, F)]) {
 
     if(i != numPC) {
 
-        List_plot[[paste(i)]] <- PCA_plot(pca, ind, variable1, c(i, i+1))
+        List_plot[[paste(i)]] <- PCA_plot(pca, ind, variable1, variable2, c(i, i+1))
 
     } else {
 
-        List_plot[[paste(i)]] <- PCA_plot(pca, ind, variable1, c(i, i+1))
+        List_plot[[paste(i)]] <- PCA_plot(pca, ind, variable1, variable2, c(i, i+1))
     }
 
     ggsave(paste0(output, "_", (i+1)/2, ".png"),
