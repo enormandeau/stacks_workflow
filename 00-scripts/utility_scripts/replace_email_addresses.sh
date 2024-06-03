00a61a4835bb0a6a4269f799e9f4a6622fb8253b
Eric Normandeau <eric.normandeau.qc@gmail.com>
2024-06-03 08:31:54 -0400

    Remove replace_email_addresses.sh

--- 00-scripts/utility_scripts/replace_email_addresses.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Replace string 'your.email@service.com' by content of $YOUREMAILADDRESS
-# Set this last variable in your ~/.bashrc file
-
-for script in 00-scripts/katak_jobs/*.sh
-do
-    grep "your.email@service.com" "$script"
-    perl -i -sape 's/YOUREMAIL/$e/' -- -e="$YOUREMAIL" "$script"
-done
