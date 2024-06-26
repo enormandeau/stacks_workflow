99250b90a555a3fbf66ccbc39fe9a42720baa386
Eric Normandeau <eric.normandeau.qc@gmail.com>
2024-06-26 12:43:33 -0400

    Display group names vertically

--- 00-scripts/utility_scripts/plot_heterozygozity.R
+++ 00-scripts/utility_scripts/plot_heterozygozity.R
@@ -10,12 +10,16 @@ output_figure = paste0(input_file, ".graph.png")
 data = read.table(input_file, header=T, stringsAsFactors=F)
 
 # Figure
-png(output_figure, width=1000, height=400)
+png(output_figure, width=1000, height=600)
+par(mar=c(12, 4, 2, 2) + 0.1)
 
 boxplot(data[,1] ~ data[,3],
         outpch=19,
         outcol="black",
         main="Boxplot of heterozygosity per samples regrouped by populations",
-        xlab="Population",
-        ylab="Heterozygosity from vcftools")
+        xlab="",
+        xaxt="n",
+        ylab="Heterozygosity by group from vcftools")
+
+axis(1, at=1:length(unique(data[,3])), las=2, labels=unique(data[,3]))
 dev.off()
