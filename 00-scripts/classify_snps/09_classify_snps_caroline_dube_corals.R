e6f8842c0813c10d455daa82391e3727cf6cb219
Eric Normandeau <eric.normandeau@qc.gmail.com>
2025-03-07 09:52:10 -0500

    Replace singleton by canonical

--- 00-scripts/classify_snps/09_classify_snps_caroline_dube_corals.R
+++ 00-scripts/classify_snps/09_classify_snps_caroline_dube_corals.R
@@ -9,15 +9,15 @@ output_file = paste0(input_file, ".categorized")
 data = read.table(input_file, header=T, stringsAsFactors=F)
 d = data[,c("MedRatio", "PropHet", "PropHomRare", "Fis", "MedCovHet", "MedCovHom")]
 
-singleton =     "#00000011" # black
+canonical =     "#00000011" # black
 duplicated =    "#FF000022" # red
 diverged =      "#0000FF22" # blue
 lowconf =       "#DD00BB22" # purple
 highcov =       "#00AA0022" # green
 mas =           "#FFAA0022" # orange
 
-# All loci marked singleton before filters
-d$Color = singleton
+# All loci marked canonical before filters
+d$Color = canonical
 
 # Fis is too negative = duplicated
 d$Color[d$Fis < -0.4] = duplicated
@@ -45,12 +45,12 @@ d$Color[d$MedCovHom > 200 | d$MedCovHet > 200] = highcov
 d$Color[data$NumHet + data$NumRare < 3] = mas
 
 # Extract bad loci infos
-bad_snps = d$Color != singleton
+bad_snps = d$Color != canonical
 all_loci = unique(gsub("_.*", "", data$ID))
 bad_loci = unique(gsub("_.*", "", data$ID[bad_snps]))
 
 # Categorize SNPs to filter loci with next script
-data$Category = "singleton"
+data$Category = "canonical"
 data$Category[d$Color == duplicated] = "duplicated"
 data$Category[d$Color == mas] = "mas"
 data$Category[d$Color == diverged] = "diverged"
@@ -74,7 +74,7 @@ png(paste0(input_file, "_2.png"), width=1200, height=950)
     plot(d$PropHet, d$MedRatio, pch=19, cex=1.5, col=d$Color, xlim=c(0, 1), ylim=c(0, 0.8))
 invisible(dev.off())
 
-single = d[data$Category == "singleton", ]
+single = d[data$Category == "canonical", ]
 png(paste0(input_file, "_3.png"), width=1200, height=950)
     plot(single$PropHet,
          single$MedRatio,
