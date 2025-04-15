640ffe436f06b31f3f1c4da4d76d6dc43eae0bef
Eric Normandeau <eric.normandeau@qc.gmail.com>
2025-04-15 10:19:14 -0400

    Permit other genotype symbols, ex: 012

--- 00-scripts/utility_scripts/vcf_to_csv.py
+++ 00-scripts/utility_scripts/vcf_to_csv.py
@@ -2,7 +2,11 @@
 """Convert STACKS VCF to simplified csv file with genotypes
 
 Usage:
-    <program> input_vcf output_csv
+    <program> input_vcf output_csv [genotype_symbols]
+
+Where:
+    genotype_symbols is a string or 3 symbols to be used to represent 0/0, 0/1 or 1/0, and 1/1
+    Example1: "012"  (for frequent, homozygote, rare)
 """
 
 # Modules
@@ -17,12 +21,35 @@ def myopen(_file, mode="rt"):
     else:
         return open(_file, mode=mode)
 
+def correct_symbol(s, s1, s2, s3):
+    assert s in ("0/0", "0/1", "1/0", "1/1", "./."), f"Bad genotype encoutered: {s}"
+
+    if s == "0/0":
+        return s1
+    elif s in ("0/1", "1/0"):
+        return s2
+    elif s == "1/1":
+        return s2
+    elif s == "./.":
+        return "NA"
+
 # Parse user input
 try:
     input_vcf = sys.argv[1]
     output_csv = sys.argv[2]
 except:
     print(__doc__)
+    sys.exit(1)
+
+try:
+
+    s1, s2, s3 = list(sys.argv[3])
+    print(f"Using the following genotype symbols: {s1}, {s2}, {s3}")
+except:
+    print(__doc__)
+    print()
+    print("ERROR: Bad genotype symbols")
+    print(s1, s2, s3)
     sys.exit(1)
 
 # Convert
@@ -42,6 +69,10 @@ with myopen(input_vcf, "rt") as infile:
 
                 if ":" in line:
                     data = [x.split(":")[0] for x in data]
+
+                    # Correct genotype symbols
+                    if s1:
+                        data = [correct_symbol(x, s1, s2, s3) for x in data]
 
                 to_write += data
 
